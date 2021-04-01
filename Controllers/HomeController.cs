dc3d69075465953e54670e45b1b531c4ed487081
mathieu.darribau@ynov.com <mathieu.darribau@ynov.com>
2021-04-01 11:25:04 +0200

    début de la page d'accueil

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BridgeMonitor.Models;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace BridgeMonitor.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            List<BoatModel> boats = GetBoats();
+            return View(boats);
+        }
+
+        public IActionResult All()
+        {
+            List<BoatModel> boats = GetBoats();
+            return View(boats);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public static List<BoatModel> GetBoats()
+        {
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync("https://api.alexandredubois.com/pont-chaban/api.php");
+                var stringResult = response.Result.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<List<BoatModel>>(stringResult.Result);
+                return result;
+            }
+        }
+    }
+}
